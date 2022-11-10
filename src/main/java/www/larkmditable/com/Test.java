e90441ba147535548ab20563f215875e2f02ba8a
wxgzgl <wxgzgl@gmail.com>
2022-11-10 11:52:56 +0800

    初始化项目

---
+++ src/main/java/www/larkmditable/com/Test.java
@@ -0,0 +1,14 @@
+package www.larkmditable.com;
+
+/**
+ *
+ *
+ * @Date: 2022/11/10 11:51
+ * @Description:
+ **/
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello honnghu");
+	}
+}
