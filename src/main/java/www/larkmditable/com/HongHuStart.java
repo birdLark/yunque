35cfe59c6a8204ae54dab0fb16d2e6a755da96c6
wxgzgl <wxgzgl@gmail.com>
2022-11-10 14:28:32 +0800

    Create HongHuStart.java

---
+++ src/main/java/www/larkmditable/com/HongHuStart.java
@@ -0,0 +1,13 @@
+package www.larkmditable.com;
+
+/**
+ *
+ *
+ * @Date: 2022/11/10 14:28
+ * @Description:
+ **/
+public class HongHuStart {
+	public static void main(String[] args) {
+		System.out.println("hello honghu!!!");
+	}
+}
