e31fad1a6ccd7a39094612bf166cc483dc52df38
wxgzgl <wxgzgl@gmail.com>
2022-11-10 14:17:53 +0800

    Create DorisWriter.java

---
+++ src/main/java/www/larkmditable/com/writer/DorisWriter.java
@@ -0,0 +1,13 @@
+package www.larkmditable.com.writer;
+
+/**
+ *
+ *
+ * @Date: 2022/11/10 14:17
+ * @Description:
+ **/
+public class DorisWriter {
+	public static void main(String[] args) {
+
+	}
+}
