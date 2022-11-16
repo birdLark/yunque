108b54ea166508ffd75158c6db7cfdcbc511fd64
baiyaoming <baiyaoming@bym-matebook14s>
2022-11-16 10:31:00 +0800

    编写测试函数

--- starrocks-reader/src/main/java/www/larkmidtable/com/StartRockerReader.java
+++ starrocks-reader/src/main/java/www/larkmidtable/com/StartRockerReader.java
@@ -26,14 +26,27 @@ public class StartRockerReader extends Reader {
 	private Connection connection ;
 	private PreparedStatement statement ;
 	private static Logger logger = LoggerFactory.getLogger(StartRockerReader.class);
+	
+	
+	public static void main(String[] args) {
+		StartRockerReader r = new StartRockerReader();
+		r.open();
+		Queue<List<String>> q =  r.startRead(null);
+		List<String> l = q.poll();
+		for(String n : l) {
+			System.out.println(n);
+		}
+		
+	}
+	
 	@Override
 	public void open() {
 		try {
-			logger.info("MySQL的Reader建立连接开始....");
+			logger.info("starrocks的Reader建立连接开始....");
 			Class.forName(DBType.MySql8.getDriverClass());
 			connection = DriverManager
-					.getConnection("jdbc:mysql://localhost:3306/filedb?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC","root","root");
-			logger.info("MySQL的Reader建立连接结束....");
+					.getConnection("jdbc:mysql://10.28.60.132:9030/example_db?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC", "root", "root");
+			logger.info("starrocks的Reader建立连接结束....");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -41,39 +54,39 @@ public class StartRockerReader extends Reader {
 
 	@Override
 	public Queue<List<String>> startRead(String[] inputSplits) {
-		logger.info("MySQL读取数据操作....");
+		logger.info("starrocks读取数据操作....");
 		try {
 			List<String> records =  new ArrayList<>();
-			String sql = "select * from student";
+			String sql = "select * from table1";
 			statement = connection.prepareCall(sql);
 			ResultSet resultSet = statement.executeQuery();
 			while (resultSet.next()) {
-				String name = resultSet.getString("name");
+				String name = resultSet.getString("username");
 				records.add(name);
 			}
 			getQueue().add(records);
 		}catch (Exception e){
 			e.printStackTrace();
 		}
-		logger.info("MySQL读取数据结束....");
+		logger.info("starrocks读取数据结束....");
 		return getQueue();
 	}
 
 
 	@Override
 	public String[] createInputSplits() {
-		logger.info("MySQL的Reader开始进行分片开始....");
-		logger.info("MySQL的Reader开始进行分片结束....");
+		logger.info("starrocks的Reader开始进行分片开始....");
+		logger.info("starrocks的Reader开始进行分片结束....");
 		return new String[5];
 	}
 
 	@Override
 	public void close()  {
 		try {
-			logger.info("MySQL的Reader开始进行关闭连接开始....");
+			logger.info("starrocks的Reader开始进行关闭连接开始....");
 			statement.close();
 			connection.close();
-			logger.info("MySQL的Reader开始进行关闭连接结束....");
+			logger.info("starrocks的Reader开始进行关闭连接结束....");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
