98f25486b5af944fafbad26c0ff048b75c9a8e67
ZFCode <zhangfei0210@163.com>
2022-11-22 19:06:14 +0800

    sqlserver reader and writer update config

---
+++ sqlserverreader/file/sqlserver_reader_readme.md
@@ -0,0 +1,28 @@
+# 1.配置文件的配置
+
+```
+writer:
+  plugin: "sqlserverreader"
+  url: "jdbc:sqlserver://localhost:1433;DatabaseName=ILS"
+  username: "manh"
+  password: "1qaz@WSX"
+  table: "student"
+  column: "id,name"
+```
+
+# 2.参数的说明
+
+| 参数名称 | 参数描述       |
+| -------- | -------------- |
+| plugin   | 插件的名称     |
+| url      | 请求访问的路径 |
+| username | 用户名         |
+| password | 密码           |
+| table    | 读取的表       |
+| column   | 读取的列       |
+
+
+
+# 3.依赖的JAR包的位置
+
+暂无依赖的JAR包
--- sqlserverreader/src/main/java/www/larkmidtable/com/reader/sqlserverreader/SqlServerReader.java
+++ sqlserverreader/src/main/java/www/larkmidtable/com/reader/sqlserverreader/SqlServerReader.java
@@ -1,76 +1,88 @@
 package www.larkmidtable.com.reader.sqlserverreader;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import www.larkmidtable.com.channel.Channel;
-import www.larkmidtable.com.reader.Reader;
-import www.larkmidtable.com.util.DBType;
-import www.larkmidtable.com.util.DBUtil;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import www.larkmidtable.com.channel.Channel;
+import www.larkmidtable.com.reader.AbstractDBReader;
+import www.larkmidtable.com.util.DBType;
+import www.larkmidtable.com.util.DBUtil;
 
 /**
  * @author fei
  * @description: sqlserver 读工具
  * @date 2022-11-15
  */
-public class SqlServerReader extends Reader {
-    private Connection connection ;
-    private PreparedStatement statement ;
-    private static Logger logger = LoggerFactory.getLogger(SqlServerReader.class);
+public class SqlServerReader extends AbstractDBReader {
+	private Connection connection;
+	private static Logger logger = LoggerFactory.getLogger(SqlServerReader.class);
 
-    @Override
-    public void open() {
-        logger.info("SqlServer的Reader建立连接开始....");
+	@Override
+	public void open() {
+		logger.info("SqlServer的Reader建立连接开始....");
+		try {
+			connection = DBUtil.getConnection(DBType.SQLSERVER.getDriverClass(), configBean.getUrl(),
+					configBean.getUsername(), configBean.getPassword());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		logger.info("SqlServer的Reader建立连接结束....");
+	}
 
-        try {
-        	String url="jdbc:sqlserver://localhost:1433;DatabaseName=ILS";
-            String username="manh";
-            String password="1qaz@WSX";
-            DBUtil.getConnection(DBType.SQLSERVER.getDriverClass(),url,username,password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        logger.info("SqlServer的Reader建立连接结束....");
-    }
+	@Override
+	public Queue<List<String>> startRead(String[] inputSplits) {
+		logger.info("SqlServer读取数据操作....");
+		try {
+			if (inputSplits.length > 1) {
+				// 开启多线程肚
+				batchStartRead(connection, inputSplits);
+			} else {
+				defaultSingleStartRead(connection, inputSplits[0]);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		logger.info("SqlServer读取数据结束....");
+		return Channel.getQueue();
+	}
 
-    @Override
-    public Queue<List<String>> startRead(String[] inputSplits) {
-        logger.info("SqlServer读取数据操作....");
-        try {
-            List<String> records =  new ArrayList<>();
-            String sql = "select * from student";
-            statement = connection.prepareCall(sql);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                String name = resultSet.getString("name");
-                records.add(name);
-            }
-			Channel.getQueue().add(records);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        logger.info("SqlServer读取数据结束....");
-        return Channel.getQueue();
-    }
+	@Override
+	public String[] createInputSplits() {
+		logger.info("SqlServer的Reader开始进行分片开始....");
+		String inputSql = String.format("select %s from %s", configBean.getColumn(), configBean.getTable());
+		List<String> results = defaultInputSplits(configBean.getColumn(), inputSql);
+		logger.info("SqlServer的Reader开始进行分片结束....");
+		String[] array = new String[results.size()];
+		return results.toArray(array);
+	}
 
-    @Override
-    public String[] createInputSplits() {
-        logger.info("SqlServer的Reader开始进行分片开始....");
-        logger.info("SqlServer的Reader开始进行分片结束....");
-        return new String[5];
-    }
+	@Override
+	public void close() {
+		logger.info("SqlServer的Reader开始进行关闭连接开始....");
+		DBUtil.close(connection);
+		logger.info("SqlServer的Reader开始进行关闭连接结束....");
+	}
 
-    @Override
-    public void close() {
-        logger.info("SqlServer的Reader开始进行关闭连接开始....");
-        DBUtil.close(statement,connection);
-        logger.info("SqlServer的Reader开始进行关闭连接结束....");
-    }
+	@Override
+	public int count() {
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = connection.prepareStatement("SELECT count(*) FROM " + configBean.getTable());
+			ResultSet resultSet = preparedStatement.executeQuery();
+			resultSet.next();
+			return resultSet.getInt(1);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		} finally {
+			DBUtil.close(preparedStatement);
+		}
+		return 0;
+	}
 }
