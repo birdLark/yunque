6b5046f85a659d6c4be37bba17219c531f203f66
wxgzgl <wxgzgl@gmail.com>
2022-12-05 21:35:18 +0800

    add jars

---
+++ jars/install_jars.bat
@@ -0,0 +1,3 @@
+call mvn install:install-file -DgroupId=com.github.noraui -DartifactId=ojdbc6 -Dversion=11.2.0.3 -Dpackaging=jar -Dfile=../jars/ojdbc6-11.2.0.3.jar
+
+call mvn install:install-file -DgroupId=com.kingbase8 -DartifactId=kingbase8 -Dversion=8.6.0 -Dpackaging=jar -Dfile=../jars/kingbase8-8.6.0.jar
