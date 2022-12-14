4395d8c110503bdeb4396940283ef32dd10624b2
lqz <315457522@qq.com>
2022-12-14 14:16:03 +0800

    init start script

---
+++ bin/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 脚本环境变量
+export WORKPATH=$( cd $(dirname $0)/../ ;pwd)
+export LIBPATH="${WORKPATH}/lib"
+
+# java 环境变量
+export JAVACMD=$(which java| awk '{print $1 }')
+
+# 启动命令
+${JAVACMD} -server -cp "${LIBPATH}/*"  com.larkmidtable.yunque.YunQueStart
