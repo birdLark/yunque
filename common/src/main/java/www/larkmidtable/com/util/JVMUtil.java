eccb3d354cfe07ff202f71797b1bcbfa10849885
wxgzgl <wxgzgl@gmail.com>
2023-04-29 12:55:27 +0800

    优雅关闭多线程

---
+++ common/src/main/java/www/larkmidtable/com/util/JVMUtil.java
@@ -0,0 +1,45 @@
+package www.larkmidtable.com.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ *
+ * @Date: 2023/4/29 12:48
+ * @Description:
+ **/
+public class JVMUtil {
+
+	private static Logger logger = LoggerFactory.getLogger(JVMUtil.class);
+
+	public static void shutdownThreadPool(ExecutorService threadPool) {
+		Runtime.getRuntime().addShutdownHook(new Thread(){
+			@Override
+			public void run() {
+				try {
+					logger.info("开始销毁线程池...");
+					try {
+						// 等待未完成任务结束
+						if (!threadPool.awaitTermination(5, TimeUnit.SECONDS)) {
+							threadPool.shutdownNow(); // 取消当前执行的任务
+							if (!threadPool.awaitTermination(5, TimeUnit.SECONDS))
+								logger.error("销毁线程池失败...");
+						}
+					} catch (InterruptedException ie) {
+						threadPool.shutdownNow();
+						logger.error("销毁线程池...");
+						Thread.currentThread().interrupt();
+					}
+					logger.info("成功销毁线程池....");
+					TimeUnit.SECONDS.sleep(2);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
--- core/src/test/java/ThreadTest.java
+++ core/src/test/java/ThreadTest.java
@@ -2,9 +2,12 @@ import com.larkmidtable.yunque.YunQueXMLStart;
 import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import www.larkmidtable.com.util.JVMUtil;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -16,7 +19,7 @@ public class ThreadTest {
 	// 定义日志对象
 	private static Logger logger = LoggerFactory.getLogger(YunQueXMLStart.class);
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException, InterruptedException {
 
 		logger.info("Hello! 云雀!");
 
@@ -24,18 +27,23 @@ public class ThreadTest {
 
 		// 2.线程池多线程提交任务
 		ExecutorService executor = Executors.newFixedThreadPool(10);
+		CountDownLatch countDownLatch = new CountDownLatch(10);
 		for (int i = 0; i < 10; i++) {
 			executor.submit(() -> {
-				System.out.println("任务处理线程ID: " + Thread.currentThread().getId()+"读取记录");
+				logger.info("任务处理线程ID: " + Thread.currentThread().getId()+"读取记录");
 				try {
 					Thread.sleep(1000L);
+					countDownLatch.countDown();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			});
 		}
-		logger.info("结束迁移任务!");
-		// 关闭线程池
-		executor.shutdown();
+		//
+		JVMUtil.shutdownThreadPool(executor);
+		// 主线程在阻塞，当计数器==0，就唤醒主线程往下执行。
+		countDownLatch.await();
+		System.exit(0);
 	}
+
 }
