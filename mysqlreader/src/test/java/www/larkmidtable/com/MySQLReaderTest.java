94239e41f69a0be9e1de3e366e6a26ca6e4967af
15901296447 <wangsk5@asiainfo.com>
2023-05-26 22:54:01 +0800

    wangshengkai add H2 for junit test

--- mysqlreader/src/test/java/www/larkmidtable/com/MySQLReaderTest.java
+++ mysqlreader/src/test/java/www/larkmidtable/com/MySQLReaderTest.java
@@ -1,11 +1,44 @@
 package www.larkmidtable.com;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import www.larkmidtable.com.bean.ConfigBean;
 
+import java.sql.*;
+
 public class MySQLReaderTest {
     private MySQLReader mySQLReader = new MySQLReader();
+    private Connection conn = null;
+    private Statement stmt = null;
+
+    @Before
+    public void before() throws SQLException {
+        conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+        stmt = conn.createStatement();
+    }
+
+    @Test
+    public void shouldTestH2() throws SQLException {
+        try{
+            stmt.execute("DROP TABLE MY_USER"); //删除已经存在的表
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        stmt.execute("CREATE TABLE MY_USER(ID VARCHAR(10) PRIMARY KEY,NAME VARCHAR(50))"); //创建表
+        stmt.executeUpdate("INSERT INTO MY_USER VALUES('001','刘备')"); //插入数据
+        stmt.executeUpdate("INSERT INTO MY_USER VALUES('002','关羽')");
+        stmt.executeUpdate("INSERT INTO MY_USER VALUES('003','张飞')");
+
+        ResultSet rs = stmt.executeQuery("SELECT NAME FROM MY_USER WHERE ID='001'"); //查询
+        while(rs.next()){
+            String name = rs.getString("NAME");
+            Assert.assertTrue(name.equals("刘备"));
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
 
     @Before
     public void init() {
